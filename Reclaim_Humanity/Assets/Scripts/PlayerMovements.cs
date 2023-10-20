48a938a68d23f22d4586b9055f145221d1077379
Iacopo2199 <iacoporoberto.ferrario@mail.polimi.it>
2023-10-20 08:41:51 +0100

    Player flip

--- Reclaim_Humanity/Assets/Scripts/PlayerMovements.cs
+++ Reclaim_Humanity/Assets/Scripts/PlayerMovements.cs
@@ -7,6 +7,7 @@ public class PlayerMovements : MonoBehaviour {
     [SerializeField] private float speed = 420.0f;
     private Vector2 movement = Vector2.zero;
     private Rigidbody2D rb = null;
+    private float horizontalInput;
     // Start is called before the first frame update
     void Awake() {
         rb = GetComponent<Rigidbody2D>();
@@ -14,7 +15,14 @@ public class PlayerMovements : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate() {
-        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        movement = new Vector2(horizontalInput, Input.GetAxisRaw("Vertical"));
         rb.velocity = speed * Time.fixedDeltaTime * movement;
+
+        if (horizontalInput > 0.01f)  //se mi muovo a destra
+            transform.localScale = new Vector2(1, 1);
+        if (horizontalInput < -0.01f)  //se mi muovo a sinistra
+            transform.localScale = new Vector2(-1, 1);
     }
 }
