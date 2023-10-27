d0e909a9637ae19be2e696d9e96e81e63d81a7ae
Michele Bosio <michele2.bosio@mail.polimi.it>
2023-10-27 14:08:23 +0200

    Battery Updated

--- Reclaim_Humanity/Assets/Scripts/Items Interaction/Laboratory Scene/ShowEnergyLab.cs
+++ Reclaim_Humanity/Assets/Scripts/Items Interaction/Laboratory Scene/ShowEnergyLab.cs
@@ -6,27 +6,25 @@ using UnityEngine.UI;
 public class ShowEnergyLab : MonoBehaviour {
     
     // private bool isTriggered = false;
-    private TextMeshProUGUI textEnergy;
     private int energy;
     [SerializeField] private LabEnergySO labEnergy;
-    [SerializeField] private Canvas canvas;
-    
+    [SerializeField] private TextMeshProUGUI textEnergy;
+    private Canvas canvas;
     
     // Start is called before the first frame update
     void Awake() {
-        energy = (int) labEnergy.totalEnergy;
-        textEnergy = GetComponent<TextMeshProUGUI>();
+        canvas = GetComponent<Canvas>();
         canvas.enabled = false;
     }
 
+    private void Update() { energy = (int) labEnergy.totalEnergy; }
+
     private void OnTriggerEnter2D(Collider2D other) {
         textEnergy.text = energy.ToString();
-        // isTriggered = true;
         canvas.enabled = true;
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        // isTriggered = false;
         canvas.enabled = false;
     }
 }
