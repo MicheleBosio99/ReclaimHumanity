69daaa40ce82c93ae2ac30c143ecdf119853fdca
riccardomalpiedi <S1092879@studenti.univpm.it>
2023-10-31 21:43:29 +0100

    Combat scene
    First sketch

---
+++ Reclaim_Humanity/Assets/Scripts/Battle/BattleHud.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHud : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI playerText;
+    [SerializeField] private HPBar hpBar;
+}
