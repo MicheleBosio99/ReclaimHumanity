cdbfdac039757490f66715fdd198143eb10089a7
mrPesci <andrea1.pesci@mail.polimi.it>
2023-12-04 17:43:08 +0100

    Wallo correct moves!!!!

--- Reclaim_Humanity/Assets/Scripts/Main Character/PlayerActions.cs
+++ Reclaim_Humanity/Assets/Scripts/Main Character/PlayerActions.cs
@@ -7,8 +7,8 @@ using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour {
 
-    [SerializeField] private float normalSpeed = 8.0f;
-    private float currentSpeed = 8.0f;
+    [SerializeField] private float normalSpeed;
+    private float currentSpeed = 0f;
 
     public float NormalSpeed {
         get => normalSpeed;
@@ -20,16 +20,45 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     private Vector2 movingDirection = Vector2.zero;
+    [SerializeField] private Vector2 movement;
     private Rigidbody2D rb;
+    public Animator animator;
     
     private void Start() {
         // DontDestroyOnLoad(gameObject);
         
         rb = GetComponent<Rigidbody2D>();
-        currentSpeed = normalSpeed;
+        
     }
 
-    public void FixedUpdate() { rb.velocity = currentSpeed * movingDirection; }
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-    public void Move(InputAction.CallbackContext context) { movingDirection = context.ReadValue<Vector2>(); }
+        if (movement != Vector2.zero)
+        {
+           animator.SetFloat("Horizontal", movement.x); 
+           animator.SetFloat("Vertical", movement.y); 
+        }
+        
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+	public void Move(InputAction.CallbackContext context) { 
+		movingDirection = context.ReadValue<Vector2>(); 
+        print(movingDirection);
+	}
+
+    public void FixedUpdate() { 
+		
+        currentSpeed = normalSpeed; //normal movement
+        //new method
+        //rb.MovePosition(rb.position * movement * normalSpeed * Time.fixedDeltaTime); 
+        
+        rb.velocity = currentSpeed * movingDirection; //old method
+        
+	}
+
+    
 }
